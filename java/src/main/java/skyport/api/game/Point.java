e472cd75b13029aabffe662888108eaa1aa2e80b
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-10-06 22:47:11 +0200

    add distance calculation of points

--- java/src/main/java/skyport/api/game/Point.java
+++ java/src/main/java/skyport/api/game/Point.java
@@ -15,5 +15,25 @@ public class Point {
     
     public int getJ() {
 	return j;
+    public int distance(Point point) {
+        int dk = Math.abs(k - point.k);
+        int dj = Math.abs(j - point.j);
+
+        int distance = 0;
+        if (dk < dj / 2) {
+            distance = dj;
+        } else {
+            distance = (int) (dj + dk - Math.floor(dj / 2.0));
+        }
+
+        if (j % 2 == 0) {
+            if (dj % 2 == 1 && k > point.k) {
+                distance--;
+            }
+        } else if (dj % 2 == 1 && k < point.k) {
+            distance--;
+        }
+
+        return distance;
     }
 }
