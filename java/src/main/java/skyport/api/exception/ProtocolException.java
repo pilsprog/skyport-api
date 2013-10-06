be6dfce714387fb1313e975f462800bc82a0fb6b
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-10-06 19:52:20 +0200

    remove unused file

--- java/src/main/java/skyport/api/exception/ProtocolException.java
+++
@@ -1,8 +0,0 @@
-package skyport.api.exception;
-
-public class ProtocolException extends RuntimeException {
-    private static final long serialVersionUID = 4551849434956754023L;
-    public ProtocolException(String reason) {
-	super(reason);
-    }
-}
