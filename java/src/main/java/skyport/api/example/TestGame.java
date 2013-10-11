bbea0ffc7bbad37465151cd165750d38bc8f2328
Eivind Jahren <eivind.jahren@ii.uib.no>
2013-10-11 20:00:23 +0200

    Added documentation

--- java/src/main/java/skyport/api/example/TestGame.java
+++ java/src/main/java/skyport/api/example/TestGame.java
@@ -1,12 +1,25 @@
 package skyport.api.example;
 
+/**
+ * 
+ * A testgame that starts two Randomplayers and a graphical
+ * interface and connects it to the game server.
+ * 
+ * @author Bjarte
+ *
+ */
 public class TestGame {
 
     public static void main(String[] args) throws InterruptedException {
+    	
+    	// Instantiates a graphic interface
         MockGraphicsClient graphics = new MockGraphicsClient("localhost", 54331);
+        
+        // Instantiates two Random Players
         RandomPlayer daneel = new RandomPlayer("Daneel", "localhost", 54321);
         RandomPlayer giskard = new RandomPlayer("Giskard", "localhost", 54321);
 
+        //Starts everything
         Thread g = new Thread(graphics);
         Thread b1 = new Thread(daneel);
         Thread b2 = new Thread(giskard);
--- java/src/main/java/skyport/api/game/Map.java
+++ java/src/main/java/skyport/api/game/Map.java
@@ -4,11 +4,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * The game map, describes the board and what is on it.
+ * @author Bjarte
+ */
 public class Map {
-    private List<List<String>> data;
+    /**
+     * The list of data on each point
+     */
+    private List<List<Tile>> data;
+    
+    /**
+     * The length of the map in the j direction. 
+     */
     private int jLength;
+    /**
+     * The length of the map in the k direction.
+     */
     private int kLength;
+    
+    public Tile getData(Point p){
+    	if(p.getJ() < 0 || p.getJ() >= kLength ||
+    	   p.getK() < 0 || p.getK() >= jLength){
+    		return Tile.VOID;
+    	}
+    	return data.get(p.getJ()).get(p.getK());
+    }
 
+    /**
+     * @param p A point on the map.
+     * @return A list of neighbors of the given point. 
+     */
     public List<Point> neighbors(Point p) {
         List<Point> points = Arrays.asList(new Point(p.getK() + 1, p.getJ() + 1), new Point(p.getK() + 1, p.getJ()), new Point(p.getK(), p.getJ() + 1), new Point(p.getK() - 1, p.getJ() - 1), new Point(p.getK() - 1, p.getJ()), new Point(p.getK(), p.getJ() - 1));
 
