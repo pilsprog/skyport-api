b13c734df3ab1aa578c1040cdfde5f7bf9dd7656
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-10-21 20:07:33 +0200

    add documents to skyportconnection

--- java/src/main/java/skyport/api/SkyportConnection.java
+++ java/src/main/java/skyport/api/SkyportConnection.java
@@ -17,11 +17,24 @@ public class SkyportConnection {
     private BufferedWriter out;
     private BufferedReader in;
 
+    /**
+     * Sets up the connection to the server.
+     * 
+     * @param host The hostname or ip-address of the server.
+     * @param port The port name the server is listening to.
+     */
     public SkyportConnection(String host, int port) {
         this.host = host;
         this.port = port;
     }
 
+    /**
+     * Connects to the server.
+     * 
+     * Sets up the writer and reader for the socket.
+     * 
+     * @return true if the connection was successful.
+     */
     public boolean connect() {
         boolean connected = true;
         try {
@@ -52,6 +65,11 @@ public class SkyportConnection {
         return connected;
     }
 
+    /**
+     * Sends the json encoded string to the server.
+     * @param json json encoded as a string.
+     * @return returns true if the send was successful.
+     */
     public boolean send(String json) {
         boolean sent = true;
         try {
@@ -65,6 +83,10 @@ public class SkyportConnection {
         return sent;
     }
 
+    /** 
+     * Blocks on read from the server.
+     * @return Returns the next json encoded string.
+     */
     public String read() {
         String json = null;
         try {
@@ -75,6 +97,9 @@ public class SkyportConnection {
         return json;
     }
 
+    /**
+     * Closes the connection to the server.
+     */
     public void close() {
         try {
             this.socket.close();
